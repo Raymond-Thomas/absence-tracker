7d6f949a9b2887fd89a31d4ad86029789e98e9b3
Raymond Thomas <rthomas2160@gmail.com>
2025-04-17 19:09:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,10 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///leave_requests.db'
-db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
-
-class LeaveRequest(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    type = db.Column(db.String(50))
-    start = db.Column(db.String(20))
-    end = db.Column(db.String(20))
-    status = db.Column(db.String(20), default='Pending')
-    submitted = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
-def index():
-    requests = LeaveRequest.query.all()
-    return render_template('index.html', requests=requests)
-
-@app.route('/submit', methods=['GET', 'POST'])
-def submit():
-    if request.method == 'POST':
-        new_request = LeaveRequest(
-            name=request.form['name'],
-            type=request.form['type'],
-            start=request.form['start'],
-            end=request.form['end']
-        )
-        db.session.add(new_request)
-        db.session.commit()
-        return redirect('/')
-    return render_template('submit.html')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run()
