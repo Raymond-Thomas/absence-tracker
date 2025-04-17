ce1e10731a005a350c136823d281ae9c0a53eeac
Raymond Thomas <rthomas2160@gmail.com>
2025-04-17 19:23:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/submit', methods=['GET', 'POST'])
+@app.route('/submit')
 def submit():
-    if request.method == 'POST':
-        # For now, just redirect back to home
-        return redirect(url_for('home'))
-    return render_template('submit.html')
+    return render_template("submit.html")
+
+if __name__ == "__main__":
+    app.run()
