017570e645ba9452c73c4d2163d2dc005c29cbcb
Raymond Thomas <rthomas2160@gmail.com>
2025-04-18 00:17:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+# Setup database
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class LeaveRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    type = db.Column(db.String(50))
+    start = db.Column(db.String(20))
+    end = db.Column(db.String(20))
 
 # Route for the home page
 @app.route('/')
