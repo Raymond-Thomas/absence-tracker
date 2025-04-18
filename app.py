8f527a10c60d5359d3bd3f761127352a509b2c10
Raymond Thomas <rthomas2160@gmail.com>
2025-04-18 00:01:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# Route for the submission page
-@app.route('/submit', methods=['POST'])
+# Route for the form submission page (handles both GET and POST)
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    name = request.form.get('name')
-    leave_type = request.form.get('type')
-    start = request.form.get('start')
-    end = request.form.get('end')
-    return render_template('submit.html', name=name, leave_type=leave_type, start=start, end=end)
-
+    if request.method == 'POST':
+        name = request.form.get('name')
+        leave_type = request.form.get('type')
+        start = request.form.get('start')
+        end = request.form.get('end')
+        return render_template('submit.html', name=name, leave_type=leave_type, start=start, end=end)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
