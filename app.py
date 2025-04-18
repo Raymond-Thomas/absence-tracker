183d1dd41f0928c86704a36883980a7eecba7d83
Raymond Thomas <rthomas2160@gmail.com>
2025-04-18 00:20:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+
 # Setup database
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
@@ -10,12 +11,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Define the LeaveRequest model
 class LeaveRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     type = db.Column(db.String(50))
     start = db.Column(db.String(20))
     end = db.Column(db.String(20))
+
+# Create the database table
+with app.app_context():
+    db.create_all()
 
 # Route for the home page
 @app.route('/')
@@ -30,11 +36,15 @@ def submit():
         leave_type = request.form.get('type')
         start = request.form.get('start')
         end = request.form.get('end')
+
+        # Save to database
+        new_request = LeaveRequest(name=name, type=leave_type, start=start, end=end)
+        db.session.add(new_request)
+        db.session.commit()
+
         return render_template('submit.html', name=name, leave_type=leave_type, start=start, end=end)
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
